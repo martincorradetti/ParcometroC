84470b832fb78814f85465a0fb9e47d62d8a9624
martincorradetti <153010947+martincorradetti@users.noreply.github.com>
2024-01-09 21:38:34 +0100

    Update parcometro.c

--- parcometro.c
+++ parcometro.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+char zona[3];
+double costoOrario;
+int ore;
+
+int main(void) {
+	printf("Inserire zona: ");
+	scanf("%s", zona);
+  
+	printf("Inserire costo orario: ");
+	scanf("%lf", &costoOrario);
+  
+	printf("Inserire numero ore di sosta: ");
+	scanf("%d", &ore);
+  
+	printf("Il ticket ha un costo totale di %.2lf\n", costoOrario * ore);
+  return 0;
+}
