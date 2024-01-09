c066bcba68f1b1e4164c99faa5e631842d79e085
martincorradetti <martin.corradetti@hotmail.com>
2024-01-09 21:25:02 +0100

    uwu

--- Parcometro2/Parcometro2.c
+++ Parcometro2/Parcometro2.c
@@ -5,16 +5,16 @@ double costoOrario;
 int ore;
 
 int main(void) {
-	puts("Inserire zona: ");
+	printf("Inserire zona: ");
 	scanf("%s", zona);
 
-	puts("Inserire costo orario");
+	printf("Inserire costo orario: ");
 	scanf("%lf", &costoOrario);
 
-	puts("Inserire numero ore di sosta");
+	printf("Inserire numero ore di sosta: ");
 	scanf("%d", &ore);
 
-	printf("Il ticket ha un costo totale di %.2lf", costoOrario * ore);
+	printf("Il ticket ha un costo totale di %.2lf\n", costoOrario * ore);
 
 }
 
